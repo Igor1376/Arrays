f29fda0f9bacc6de77be6bfb8b0a2d897dc613f9
Igor <bignick13@yandex.ru>
2024-02-12 18:46:19 +0300

    Base task DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,19 +1,100 @@
 #include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello Arrays" << endl;
+	//cout << "Hello Arrays" << endl;
+	//cout << "Введите размер массива: ";
+	////const int SIZE = 5;
+	////int arr[SIZE];
+	////cout << "Введите " << SIZE << "Элементов массива:\n";
+
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	//cin >> arr[i];
+	//}
+	//cout << "Элементы массива:\n";
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	//cout << arr[i] << "\t";
+	//}
+
+	/*cout << "Hello Arrays" << endl;
 	const int SIZE = 5;
-	int arr[SIZE] = { 3, 5, 8 };
+	int arr[SIZE] = {3, 5, 8, 10, 15};
 	cout << int() << endl;
-	//arr[2] = 2048;
-	//cout << arr[2] << endl;
-	// ввод элементов массива с клавиатуры
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << "\t";
 	}
-	cout << endl;
+	cout << endl;*/
+
+	// -=*ввести значения элементов массива с клавиатуры*=-
+	/*cout << "Введите размер массива: " << "\n";
+	const int SIZE = 5;
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		cin >> arr[i];
+	}
+	cout << "Элементы массива: \n";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}*/
+
+	// -=*Вывод элементов массива на экран в прямом порядке*=-
+	/*const int SIZE = 5;
+	int arr[SIZE];
+	cout << "Элементы массива в прямом порядке:" << endl;
+	for (int i = 0; i < SIZE; ++i) 
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;*/
+
+	// -=*Вывод элементов массива на экран в обратном порядке*=-
+	//cout << "Элементы массива в обратном порядке:" << endl;
+	//const int SIZE = 5;
+	//int arr[SIZE];
+	//for (int i = SIZE - 1; i >= 0; --i) {
+	//	cout << arr[i] << " ";
+	//}
+	//cout << endl;
+
+	// -=*Вычисление суммы элементов массива*=-
+	//int sum = 0;
+	//const int SIZE = 5;
+	//int arr[SIZE];
+	//for (int i = 0; i < SIZE; ++i) {
+	//	sum += arr[i];
+	//}
+	//// Вывод суммы элементов массива
+	//cout << "Сумма элементов массива: " << sum << endl;
+
+	// -=*Нахождение минимального и максимального значений*=-
+	//const int SIZE = 5;
+	//int arr[SIZE];
+	//int min = arr[0];
+	//int max = arr[0];
+	//for (int i = 1; i < SIZE; ++i) 
+	//{
+	//	if (arr[i] < min) 
+	//	{
+	//		min = arr[i];
+	//	}
+	//	if (arr[i] > max) 
+	//	{
+	//		max = arr[i];
+	//	}
+	//}
+	// Вывод минимального и максимального значений
+	//cout << "Минимальное значение: " << min << endl;
+	//cout << "Максимальное значение: " << max << endl;
+
+
 }
