78e3a52644006bd322c3b841e0250a9345555f79
Igor <bignick13@yandex.ru>
2024-03-03 21:33:19 +0300

    all DONE

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+#ifdef FUNK
 int Sum(int a, int b);
 int Diff(int a, int b);
 int Prod(int a, int b);
@@ -9,7 +10,7 @@ double Quote(int a, int b);
 void main()
 {
 	setlocale(LC_ALL, "");
-	int a, b; 
+	int a, b;
 	cout << "¬ведите значение с клавиатуры: "; cin >> a >> b;
 	int c = Sum(a, b);
 	cout << a << " + " << b << " = " << c << endl;
@@ -38,3 +39,61 @@ double Quote(int a, int b)
 	//Quotient - частное
 	return (double)a / b;
 }
+#endif // FUNK
+
+int Fakt(int n, int f);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n, f = 1;
+	cout << "¬ведите значение факториала: "; cin >> n;
+	for (int i = 2; i <= n; i++)
+	{
+		f *= i;
+
+	}
+	cout << Fakt(n, f) << endl;
+}
+int Fakt(int n, int f)
+{
+	return f;
+}
+
+#ifdef FUNK2
+int Sum(int a, int b);
+int Diff(int a, int b);
+int Prod(int a, int b);
+double Quote(int a, int b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b;
+	cout << "¬ведите значение с клавиатуры: "; cin >> a >> b;
+	int c = Sum(a, b);
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Diff(a, b) << endl;
+	cout << a << " * " << b << " = " << Prod(a, b) << endl;
+	cout << a << " / " << b << " = " << Prod(a, b) << endl;
+}
+
+int Sum(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+int Diff(int a, int b)
+{
+	return a - b;
+}
+int Prod(int a, int b)
+{
+	return a * b;
+}
+double Quote(int a, int b)
+{
+	return (double)a / b;
+}
+#endif // FUNK2
+
--- Hexadecimal/main.cpp
+++ Hexadecimal/main.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 using namespace std;
 
+//#define HEXA
+//#define HEXA2
+//#define HEXA3
+
 void main()
 {
+#ifdef HEXA
 	for (int i = 0; i < 256; i++)cout << i << "\t" << (char)i << endl;
 	setlocale(LC_ALL, "");
 	int decimal;
@@ -15,7 +20,48 @@ void main()
 	//decimal & 16 - получаем остаток от деления на 16
 	for (--i; i >= 0; i--)
 	{
-		cout << char(hex[i] < 10 ? hex [i] + 48 : hex[i] + 55);
+		cout << char(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
 	}
 	cout << endl;
+#endif // HEXA
+
+#ifdef HEXA2
+	for (int i = 0; i < 256; i++)cout << i << "\t" << (char)i << endl;
+	setlocale(LC_ALL, "");
+	int decimal;
+	cout << "Введите десятичное число: "; cin >> decimal;
+	const int n = 8;
+	char hex[n] = {};
+	int i = 0;
+	for (; decimal; hex[i++] = decimal & 15, decimal >>= 4);
+	//побитовый сдвиг на 4 разряда - это деление числа на 16
+	//decimal & 16 - получаем остаток от деления на 16
+	for (--i; i >= 0; i--)
+	{
+		cout << char(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
+	}
+	cout << endl;
+#endif // HEXA2
+
+#ifdef HEXA3
+
+	for (int i = 0; i < 256; i++)cout << i << "\t" << (char)i << endl;
+	setlocale(LC_ALL, "");
+	int decimal;
+	cout << "Введите десятичное число: "; cin >> decimal;
+	const int n = 8;
+	char hex[n] = {};
+	int i = 0;
+	for (; decimal; hex[i++] = decimal & 15, decimal >>= 4);
+	//побитовый сдвиг на 4 разряда - это деление числа на 16
+	//decimal & 16 - получаем остаток от деления на 16
+	for (--i; i >= 0; i--)
+	{
+		cout << char(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
+	}
+	cout << endl;
+#endif // HEXA3
+
+
+
 }
