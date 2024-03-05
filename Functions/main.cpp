0eb5d3fce0bb92adedb3bf497e7f8c04b330407e
Igor <bignick13@yandex.ru>
2024-03-05 19:17:27 +0300

    All DONE

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,33 +1,58 @@
 #include<iostream>
 using namespace std;
 
-#ifdef FUNK
-int Sum(int a, int b);
 int Diff(int a, int b);
 int Prod(int a, int b);
 double Quote(int a, int b);
+int Sum(int a, int b); // Прототип функции (Объявление функции - function declaration)
+long long int Factorial(int n);
+double Power(double a, int n);
+
+//#define CALC
+//#define FACTORIAL
+#define POWER
 
 void main()
 {
+	system("CLS");
 	setlocale(LC_ALL, "");
+#ifdef CALC
 	int a, b;
 	cout << "Введите значение с клавиатуры: "; cin >> a >> b;
-	int c = Sum(a, b);
+	int c = Sum(a, b); // использование функциии (Вызов функции - Function call) 
 	cout << a << " + " << b << " = " << c << endl;
 	cout << a << " - " << b << " = " << Diff(a, b) << endl;
 	cout << a << " * " << b << " = " << Prod(a, b) << endl;
 	cout << a << " / " << b << " = " << Quote(a, b) << endl;
+#endif // CALC
+
+#ifdef FACTORIAL
+	int n;
+	cout << "Введите число вычисления факториала: "; cin >> n;
+	cout << Factorial(n) << endl;
+#endif // FACTORIAL
+
+#ifdef POWER
+	int a; // основание степени
+	int n; // показатель степени
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введите показатель степени: "; cin >> n;
+	cout << a << " ^ " << " n " << " = " << Power(a, n) << endl;
+	system("PAUSE");
+	main();
+#endif // POWER
 
 }
 
-int Sum(int a, int b)
+int Sum(int a, int b) // Реализация функции (Определение функции - Function definition)
 {
 	int c = a + b;
 	return c;
 }
 int Diff(int a, int b)
 {
-	return a - b;
+	// Difference
+	return a - b; 
 }
 int Prod(int a, int b)
 {
@@ -39,61 +64,33 @@ double Quote(int a, int b)
 	//Quotient - частное
 	return (double)a / b;
 }
-#endif // FUNK
-
-int Fakt(int n, int f);
-
-void main()
+long long int Factorial(int n)
 {
-	setlocale(LC_ALL, "");
-	int n, f = 1;
-	cout << "Введите значение факториала: "; cin >> n;
-	for (int i = 2; i <= n; i++)
+	long long int f = 1;
+	for (int i = 1; i <= n; i++)
 	{
 		f *= i;
-
 	}
-	cout << Fakt(n, f) << endl;
-}
-int Fakt(int n, int f)
-{
 	return f;
 }
-
-#ifdef FUNK2
-int Sum(int a, int b);
-int Diff(int a, int b);
-int Prod(int a, int b);
-double Quote(int a, int b);
-
-void main()
+double Power(double a, int n)
 {
-	setlocale(LC_ALL, "");
-	int a, b;
-	cout << "Введите значение с клавиатуры: "; cin >> a >> b;
-	int c = Sum(a, b);
-	cout << a << " + " << b << " = " << c << endl;
-	cout << a << " - " << b << " = " << Diff(a, b) << endl;
-	cout << a << " * " << b << " = " << Prod(a, b) << endl;
-	cout << a << " / " << b << " = " << Prod(a, b) << endl;
+	double N = 1;
+	if (n < 0)
+	{
+		a = 1 / a;
+		n = -n;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		N *= a;
+	}
+	return N;
 }
 
-int Sum(int a, int b)
-{
-	int c = a + b;
-	return c;
-}
-int Diff(int a, int b)
-{
-	return a - b;
-}
-int Prod(int a, int b)
-{
-	return a * b;
-}
-double Quote(int a, int b)
-{
-	return (double)a / b;
-}
-#endif // FUNK2
+
+
+
+
+
 
